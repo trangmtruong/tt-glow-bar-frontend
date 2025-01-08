6f4d332084b127a6d4c772c4990970f87980b7c1
Trang T <trangmtruong@yahoo.com>
2025-01-08 12:33:41 -0800

    add isliked button

--- src/components/PhotoCard/PhotoCard.jsx
+++ src/components/PhotoCard/PhotoCard.jsx
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "../PhotoCard/PhotoCard.css";
 
-import Favorite from "../../assets/favorite.svg";
+import LikeIcon from "../../assets/like-empty.svg";
+// import IsLikedIcon from "../..assets/isliked.svg";
 
 function PhotoCard({ item }) {
+  const [isLiked, setIsLiked] = useState(false);
+  const toggleLike = () => setIsLiked((prevLiked) => !prevLiked);
   return (
     <li className="photo">
       <img src={item.imageUrl.href} alt={item.name} className="photo__img" />
       <div className="photo__overlay">
         <p className="photo__service">{item.service}</p>
         <img
-          src={Favorite}
-          alt="favorite button"
+          src={isLiked ? IsLikedIcon : LikeIcon}
+          alt={isLiked ? "unfavoritte button" : "favorite button"}
           className="photo__favorite-btn"
+          onClick={toggleLike}
         />
       </div>
     </li>
