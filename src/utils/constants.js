cf9505e20beba7a2323a2ea8d72fc5ee35fa67a0
Trang T <trangmtruong@yahoo.com>
2025-01-17 16:29:04 -0800

    edit all src for services

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -47,7 +47,8 @@ const services = [
 
         description:
           "Perfect balance of natural and glam, a mix between classic and volume techniques, giving a textured, multi-dimensional look.",
-        src: "https://calendar.app.google/5GuS27ctFHsogGkcA",
+        // src: "https://calendar.app.google/5GuS27ctFHsogGkcA",
+        src: "https://calendar.google.com/calendar/appointments/schedules/AcZssZ13_C50CKPN43rv62AVylZJF21OLOuothpUHYh98ww2ztsgprHs-_BhFuLTys709ZylgRKuYhzx?gv=true",
       },
       {
         name: "Classic Fullset",
@@ -55,7 +56,8 @@ const services = [
         price: "150",
         description:
           "Timeless and elegant, enhances your natural beauty with individual lashes for a subtle, mascara-like effect.",
-        src: "https://calendar.app.google/kQGVayuEUEjDokX67",
+        // src: "https://calendar.app.google/kQGVayuEUEjDokX67",
+        src: "https://calendar.google.com/calendar/appointments/schedules/AcZssZ2kMx-LDanVvkpQTgZ4ZyUo5hhS6Gx3sDaHF7M5ocjLfB8rsVj_cGdR44Jl0LY4Bri8zyNDrKtL?gv=true",
       },
     ],
   },
@@ -68,28 +70,32 @@ const services = [
         time: "2",
         price: "100",
         description: "2-3 weeks touch up for volume extensions",
-        src: "https://calendar.app.google/jMngknFFUU59m9Hk7",
+        // src: "https://calendar.app.google/jMngknFFUU59m9Hk7",
+        src: "https://calendar.google.com/calendar/appointments/schedules/AcZssZ3l6mBnpsy_-h10nvyq9L98j15IoN1AkF7nLt0Q7wUtfbWomYRrt-fmKBEvTQS7_V_-XoK071kb?gv=true",
       },
       {
         name: "Hybrid Fill",
         time: "2",
         price: "90",
         description: "2-3 weeks touch up for hybrid extensions",
-        src: "https://calendar.app.google/tX4FvHFs3P7Tg1kA6",
+        // src: "https://calendar.app.google/tX4FvHFs3P7Tg1kA6",
+        src: "https://calendar.google.com/calendar/appointments/schedules/AcZssZ23vAbTQCqz_22qZil6s5pOD6s66Ij4mXluTZb8_JQo_YfTmk1tXcVND9PgbhJ1csiTIMbs7LOD?gv=true",
       },
       {
         name: "Classic Fill",
         time: "1.5",
         price: "80",
         description: "2-3 weeks touch up for classic extensions",
-        src: "https://calendar.app.google/5UTYnKTsSLJPxB5P9",
+        // src: "https://calendar.app.google/5UTYnKTsSLJPxB5P9",
+        src: "https://calendar.google.com/calendar/appointments/schedules/AcZssZ38PQGGCRhD7cdU0U8ErjD6xBWHunugHS51f9N0JCH7yuKRZiLCXxq8LRVh7A8ZFyBYWtdPffl0?gv=true",
       },
       {
         name: "Mini Fill",
         time: "1",
         price: "60",
         description: "less than 10 days touchup for any extensions",
-        src: "https://calendar.app.google/rxHFFMvc5UhPJiHV6",
+        // src: "https://calendar.app.google/rxHFFMvc5UhPJiHV6",
+        src: "https://calendar.google.com/calendar/appointments/schedules/AcZssZ3zf8YwrYaYrgvQWfBnsxbh2ebSPmkLubkKIWONGF_PWSfUf1DRexdPCA66vGlGvkkf_bHzPlRp?gv=true",
       },
     ],
   },
@@ -103,7 +109,8 @@ const services = [
         price: "100",
         description:
           "Semi-permanent treatment that curls and lifts your lashes for a stunning, eye-opening effect. Perfect for those seeking a low-maintenance alternative to lash extensions.",
-        src: "https://calendar.app.google/HKtzUotap7dAB8UL7",
+        // src: "https://calendar.app.google/HKtzUotap7dAB8UL7",
+        src: "https://calendar.google.com/calendar/appointments/schedules/AcZssZ0Nm7qcALcordp4aF_ijQGuwiHSQWkxsO0CvrhT4gfb6EokoJfeCuJrtdfqvs726Oa7mp1ZQOZT?gv=true",
       },
       {
         name: "add Tint",
@@ -111,7 +118,8 @@ const services = [
         price: "130",
         description:
           "Tinting enhances the color of the lashes, creating a fuller, darker, and more defined look without mascara.",
-        src: "https://calendar.app.google/CbqYmLTLPC4MNFHCA",
+        // src: "https://calendar.app.google/CbqYmLTLPC4MNFHCA",
+        src: "https://calendar.google.com/calendar/appointments/schedules/AcZssZ0A3kjczSLbu7yVPS8FKgSbjchrEaDL26IXxKLuIYfAWPLHN9aFH_VbjjVA1JwE5-kQN9Ga-GeD?gv=true",
       },
     ],
   },
@@ -125,7 +133,8 @@ const services = [
         price: "90",
         description:
           "Tame and shape your brows, designed to smooth and lift your natural hairs for a fuller, more defined, and polished look.",
-        src: "https://calendar.app.google/dGzGPKmYvPBUuSPr8",
+        // src: "https://calendar.app.google/dGzGPKmYvPBUuSPr8",
+        src: "https://calendar.google.com/calendar/appointments/schedules/AcZssZ22n724GhCgaQIc_W_90Esg38X1yzggcnYfECPbfkP11ByBtIYa6Mk__azXhdujMN6ens3Hyp3_?gv=true",
       },
       {
         name: "add Tint",
@@ -133,7 +142,8 @@ const services = [
         price: "120",
         description:
           "Enhance your brows' shape and color. Tinting defines brows that frame your face beautifully—ideal for effortless, long-lasting results.",
-        src: "https://calendar.app.google/sEd5KhMDtVeiLuo29",
+        // src: "https://calendar.app.google/sEd5KhMDtVeiLuo29",
+        src: "https://calendar.google.com/calendar/appointments/schedules/AcZssZ0LY94yL6ABwbVssKsBY10yp-rR0eUkWliX3-r3quG4AjPjHKz67TR20MlkvVAHLg-tBrtfxq3z?gv=true",
       },
     ],
   },
@@ -147,14 +157,16 @@ const services = [
         price: "15",
         description:
           "Removes unwanted hair from the upper lip for a smooth finish.",
-        src: "https://calendar.app.google/3jkwbgNtfXXSLjoY6",
+        // src: "https://calendar.app.google/3jkwbgNtfXXSLjoY6",
+        src: "https://calendar.google.com/calendar/appointments/schedules/AcZssZ2qHwPh5vSs_sggpTLDxpEV23OrBEyfmbTx15k_ld2NzgLJEM_I3qWWnkCztVOJOHopdz1W-qOD?gv=true",
       },
       {
         name: "Underarm",
         time: "less than .5",
         price: "30",
         description: "Hair removal from both underarms for clean, soft skin.",
-        src: "https://calendar.app.google/QoS6dTsmoYLXdVZc9",
+        // src: "https://calendar.app.google/QoS6dTsmoYLXdVZc9",
+        src: "https://calendar.google.com/calendar/appointments/schedules/AcZssZ1J4gUDJKbOEaqpJIXGKXZSlgeGgj1LizPFkG3aS9TSNB2LRqrPxnPgsr0CQfOj6zmV8s1It6bT?gv=true",
       },
       {
         name: "Arm",
@@ -162,7 +174,8 @@ const services = [
         price: "50",
         description:
           "Removes hair from both arms, leaving them silky and smooth.",
-        src: "https://calendar.app.google/hgoWeLY1nuaq19SM9",
+        // src: "https://calendar.app.google/hgoWeLY1nuaq19SM9",
+        src: "https://calendar.google.com/calendar/appointments/schedules/AcZssZ0j69DNvaXYfHgg4uDBfi4AhjzgND7m_mTi1aqyN1wIx2CEb2zDcn-LW5tfs7kqXVZe_ODmHFXJ?gv=true",
       },
       {
         name: "Leg",
@@ -170,7 +183,8 @@ const services = [
         price: "65",
         description:
           "Removes hair from both upper and lower legs for flawlessly smooth legs.",
-        src: "https://calendar.app.google/p8nKsxh9LTdpsYEz9",
+        // src: "https://calendar.app.google/p8nKsxh9LTdpsYEz9",
+        src: "https://calendar.google.com/calendar/appointments/schedules/AcZssZ1aBtY8yKxV5OotHUhxcs0QddHkftPuFoZJZ280FXFKh69K6LvyR5yr96-2LGonbk2spzZDphG8?gv=true",
       },
       {
         name: "Brazillian",
@@ -178,7 +192,8 @@ const services = [
         price: "90",
         description:
           "All hair is removed from the bikini area for a completely clean look.",
-        src: "https://calendar.app.google/Q5aUWvLacFcj7wMr7",
+        // src: "https://calendar.app.google/Q5aUWvLacFcj7wMr7",
+        src: "https://calendar.google.com/calendar/appointments/schedules/AcZssZ2zXoa6QI6AF6ldhIk18kchZJjFmiBg35i9BpgjJC3sy3Z46MW9HhHiyPV-nZy4ljPdt0UBjL0z?gv=true",
       },
     ],
   },
